ba13561dcce0bcbdad5b71fb6e987c5e21974701
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-30 17:59:15 -0600

    feat: added structuring and styles for Avatar component

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className="hidden xl:flex xl:max-w-none relative overflow-hidden gradient-mask-b-90">
+      <Image
+        src={'/avatar.png'}
+        width={737}
+        height={678}
+        alt="Avatar"
+        className="translate-z-0 w-full h-full"
+      />
+    </div>
+  );
 };
 
 export default Avatar;
