84ca3244944af8832884e4c60de72f33d577794c
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-30 17:12:00 -0600

    feat: added background image and avatar setup

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,18 @@ const Home = () => {
         </div>
       </div>
       {/* image */}
-      <div> image </div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* bg image */}
+        <div
+          className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right
+        xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"
+        >
+          {/* particles */}
+          <div>Particles</div>
+          {/* avatar img */}
+          <Avatar />
+        </div>
+      </div>
     </div>
   );
 };
