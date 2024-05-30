fd558d88ec0d177c0f2e60cc6eae0e296466bc8f
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-30 15:41:10 -0600

    feat: added Link, Router, NavBar structure and styling with tooltips

--- components/Nav.js
+++ components/Nav.js
@@ -8,6 +8,9 @@ import {
   HiEnvelope,
 } from 'react-icons/hi2';
 
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 // nav data
 export const navData = [
   { name: 'home', path: '/', icon: <HiHome /> },
@@ -27,7 +30,44 @@ export const navData = [
 ];
 
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+  return (
+    <nav
+      className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max
+       bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen"
+    >
+      {/* inner */}
+      <div
+        className="flex w-full xl:flex-col items-center justify-between 
+      xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max
+      py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full"
+      >
+        {navData.map((link, index) => {
+          return (
+            <Link
+              className={`${
+                link.path === pathname && 'text-accent'
+              } relative flex items-center group hover:text-accent transition-all duration-300`}
+              href={link.path}
+              key={index}
+            >
+              {/* tooltip */}
+              <div className="absolute pr-14 right-0 hidden xl:group-hover:flex">
+                <div className="bg-white relative flex text-primary items-center p-[6px] rounded-[3px]">
+                  <div className="text-[12px] leading-none font-semibold capitalize">
+                    {link.name}
+                  </div>
+                </div>
+              </div>
+
+              <div>{link.icon}</div>
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
