5f273f32680993b173a84b6dba743135d0eb4f87
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-30 16:12:44 -0600

    feat: added transition, motion container and AnimatePresence for App

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import Transition from '../components/Transition';
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
