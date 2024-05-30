b3928976c49d3485f8ca776a98577d85d4c179b6
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-30 16:55:35 -0600

    fix: fixed some styles typos

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -8,7 +8,7 @@ const ProjectsBtn = () => {
       <Link
         href={'/work'}
         className="relative w-[185px] h-[185px] flex justify-center items-center
-        bg-circleStar bg-cover bg-center bg-no-repear group"
+        bg-circleStar bg-cover bg-center bg-no-repeat group"
       >
         <Image
           src={'/rounded-text.png'}
@@ -17,8 +17,11 @@ const ProjectsBtn = () => {
           alt=""
           className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
         />
+        <HiArrowRight
+          className="absolute text-4xl group-hover:translate-x-2
+      transition-all duration-300"
+        />
       </Link>
-      <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
     </div>
   );
 };
