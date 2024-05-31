3b31bbfe3c054c39929c27eb5d6a9ce453d5ea7d
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-30 18:01:10 -0600

    feat: added styling and framer-motion containers to Avatar component in index

--- pages/index.js
+++ pages/index.js
@@ -59,12 +59,21 @@ const Home = () => {
         <div
           className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right
         xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"
+        ></div>
+        {/* particles */}
+        <div>Particles</div>
+        {/* avatar img */}
+        <motion.div
+          variants={fadeIn('down', 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32
+          lg:bottom-0 lg:right-[8%]"
         >
-          {/* particles */}
-          <div>Particles</div>
-          {/* avatar img */}
           <Avatar />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
