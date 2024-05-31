edefd508c81507401eaebfd011f12d0ed344893b
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-31 00:04:20 -0600

    feat: added structuring and styling for Contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,132 @@
+import React, { useState } from 'react';
+import { BsArrowRight } from 'react-icons/bs';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+import emailjs from 'emailjs-com';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const [formData, setFormData] = useState({
+    from_name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        'service_pj4vz6e',
+        'template_u6kvt8b',
+        {
+          from_name: formData.from_name,
+          to_name: 'Xavier',
+          message: formData.message,
+          email: formData.email,
+          subject: formData.subject,
+        },
+        'dGZgN8qa1fjl9RTPj',
+      )
+      .then((result) => {
+        console.log('Email sent successfully:', result.text);
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error.text);
+      });
+  };
+
+  return (
+    <div className="h-full bg-primary/30">
+      <div
+        className="container mx-auto py-32 text-center xl:text-left flex items-center
+      justify-center h-full"
+      >
+        <div className="flex flex-col w-full max-w-[700px] ">
+          {/* text */}
+          <motion.h2
+            variants={fadeIn('up', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2 text-center mb-12"
+          >
+            Let's <span className="text-accent">connect.</span>
+          </motion.h2>
+          {/* form */}
+          <motion.form
+            onSubmit={handleSubmit}
+            variants={fadeIn('up', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="flex-1 flex flex-col gap-6 w-full mx-auto"
+          >
+            {/* input group */}
+            <div className="flex gap-x-6 w-full">
+              <input
+                type="text"
+                placeholder="name"
+                name="from_name"
+                value={formData.from_name}
+                onChange={handleChange}
+                className="input"
+              />
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="input"
+              />
+            </div>
+            <input
+              type="text"
+              placeholder="subject"
+              name="subject"
+              value={formData.subject}
+              onChange={handleChange}
+              className="input"
+            />
+            <textarea
+              placeholder="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              className="textarea"
+            ></textarea>
+            <button
+              type="submit"
+              className="btn rounded-full border border-white/50 max-w-[170px]
+            px-8 transition-all duration-300 flex items-center justify-center
+            overflow-hidden hover:border-accent group"
+            >
+              <span
+                className="group-hover:-translate-y-[120%] group-hover:opacity-0
+              transition-all duration-500"
+              >
+                Let's talk
+              </span>
+              <BsArrowRight
+                className="-translate-y-[120%] opacity-0 group-hover:flex
+              group-hover:-translate-y-0 group-hover:opacity-100 transition-all
+              duration-300 absolute text-[22px]"
+              />
+            </button>
+          </motion.form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
