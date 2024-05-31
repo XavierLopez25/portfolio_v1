bf45c0810e077fcc3826f4e46032996eb76c9c35
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-30 23:30:03 -0600

    feat: added structuring and styling for Works page

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,54 @@
+import WorkSlider from '../../components/WorkSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn('up', 0.3)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-12"
+            >
+              My works <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn('up', 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              Here are some of the projects I've worked on, demonstrating my skills in UI/UX design,
+              frontend and backend development, and API integration. Each project reflects my
+              commitment to quality and my ability to deliver functional and aesthetically pleasing
+              solutions. Explore my works and see how I can help bring your ideas to life.
+            </motion.p>
+          </div>
+          <motion.div
+            variants={fadeIn('down', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            {/* slider */}
+            <WorkSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Work;
