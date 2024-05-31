825f467d78e1d923f5fb269b3930adbf92fe517c
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-30 22:20:55 -0600

    feat: implemented ServiceSlider component into Services page and added styling

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,55 @@
+import { serviceData } from '../../components/ServiceSlider';
+import ServiceSlider from '../../components/ServiceSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn('up', 0.3)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
+              My services <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn('up', 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              I provide a range of services including UI/UX design, frontend and backend
+              development, and API REST development. I ensure your projects are visually appealing,
+              functional, and efficient. Let's bring your ideas to life with professional
+              development solutions.
+            </motion.p>
+          </div>
+          <motion.div
+            variants={fadeIn('down', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            {/* slider */}
+            <ServiceSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Services;
